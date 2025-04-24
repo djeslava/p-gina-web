e44eab8feee5d17bad863641bb810324c1e5ed39
Luis Ángel Motta Valero <luisangel.motta@alumnos.viu.es>
2025-04-24 07:38:01 -0500

    Update login.js - test time

--- frontend/WEB 0/assets/js/login.js
+++ frontend/WEB 0/assets/js/login.js
@@ -56,7 +56,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 mostrarNotificacion("Inicio de sesión exitoso. Espera...", "success");
                 setTimeout(() => {
                     window.location.href = "dashboard.html"; // Redirigir tras 2 segundos
-                }, 2000);
+                }, 30000);
             } else {
                 mostrarNotificacion(result.error || "Error inesperado", "error");
             }
